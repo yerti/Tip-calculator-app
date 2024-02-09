54b9652213930ecd4b743a295b4152ee2cb5c925
yerti <yerti0818@gmail.com>
2024-02-08 20:37:13 -0500

    Modifi file

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE = "static";
+const STATIC_CACHE = "static-v1";
 
 const APP_SHELL = [
     "/",
@@ -7,20 +7,25 @@ const APP_SHELL = [
     "images/icon-dollar.svg",
     "images/icon-person.svg",
     "images/logo.svg"
-]
-self.addEventListener("install", e => {
-    const cacheStatic = caches.open(STATIC_CACHE).then((cache) => cache.addAll(APP_SHELL));
+];
 
-    e.waitUntil(cacheStatic);
-})
+self.addEventListener("install", (e) => {
+  const cacheStatic = caches
+    .open(STATIC_CACHE)
+    .then((cache) => cache.addAll(APP_SHELL));
 
-self.addEventListener("fetch", e => {
-    console.log("fetch!", e.request);
+  e.waitUntil(cacheStatic);
+});
 
-    e.respondWith(
-        caches
-            .match(e.request)
-            .then((res) => res || fetch(e.request))
-            .catch(console.log)
-    );
-})
+self.addEventListener("fetch", (e) => {
+  console.log("fetch! ", e.request);
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((res) => {
+        return res || fetch(e.request);
+      })
+      .catch(console.log)
+  );
+  //   e.waitUntil(response);
+});
